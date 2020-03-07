a565ae048ceebfa3a3e86499784d7a8e8116b46d
unknown <rbrtmt100@gmail.com>
2020-03-07 20:51:43 +0300

    Add author info and question creation date in question item

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -15,9 +15,12 @@ class QuestionController extends Controller
     public function index()
     {
         //
-		$questions=Question::latest()->paginate(5);
+		//\DB::enableQueryLog();
+		$questions=Question::with('user')->latest()->paginate(5);
 		
-		return view('questions.index', compact('questions'));
+	     //view('questions.index', compact('questions'))->render();
+		 return view('questions.index', compact('questions'));
+		 //dd(\DB::getQueryLog());
     }
 
     /**
