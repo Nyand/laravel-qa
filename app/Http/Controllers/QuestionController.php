0bb446b22c5610352e3258518123c108b2c636fd
unknown <rbrtmt100@gmail.com>
2020-03-08 20:57:50 +0300

    Build question form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -31,6 +31,8 @@ class QuestionController extends Controller
     public function create()
     {
         //
+		$question = new Question();
+		return view('questions.create', compact('question'));
     }
 
     /**
