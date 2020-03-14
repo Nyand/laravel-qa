cddef1b5a895056436f1019da504bd1eee4f5571
unknown <rbrtmt100@gmail.com>
2020-03-14 16:11:43 +0300

    Add vote control to question and answer item

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -4,18 +4,36 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-				<div class="d-flex align-items-center">
-				<h1>{{ $question->title}}</h1>
-				<div class="ml-auto">
-					<a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to All Question</a>
-				</div>
-				</div>
+          <div class="card">
+            <div class="card-body">
+				<div class="card-title">
+					<div class="d-flex align-items-center">
+					<h1>{{ $question->title}}</h1>
+							<div class="ml-auto">
+								<a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to All Question</a>
+							</div>
+					</div>
                 </div>
-                <div class="card-body">
-				{!! $question->body_html !!}
-				<div class="float-right">
+				<hr>
+                <div class="media">
+					<div class="d-fex flex-column vote-controls">
+						<a title="This question is useful" class="vote-up">
+							<i class="fas fa-caret-up fa-3x"></i>
+						</a>
+						<span class="vote-count">1230</span>
+						<a title="This question is not useful" class="vote-down off">
+							<i class="fas fa-caret-down fa-3x"></i>
+						</a>
+						<a title="Click to mark as favorite question (Click again to undo)"
+						class="favorite mt-2 favorited"><i class="fas fa-star fa-2x"></i>
+						<span class="favorite-count">123</span>
+						</a>
+						
+					</div>
+				
+				    <div class="media-body">
+					    {!! $question->body_html !!}
+				        <div class="float-right">
 							<span class="text-muted">Asked {{$question->created_date}}</span>
 							<div class="media">
 								<a href="{{$question->user->url}}" class="pr-2">
@@ -26,8 +44,10 @@
 								</div>
 							</div>
 						</div>
+				    </div>
                 </div>
-			  </div>
+			</div>
+		  </div>
             
         </div>
     </div>
@@ -40,6 +60,21 @@
 				<hr>
 				@foreach($question->answers as $answer)
 					<div class='media'>
+					
+					<div class="d-fex flex-column vote-controls">
+						<a title="This answer is useful" class="vote-up">
+							<i class="fas fa-caret-up fa-3x"></i>
+						</a>
+						<span class="vote-count">1230</span>
+						<a title="This answer is not useful" class="vote-down off">
+							<i class="fas fa-caret-down fa-3x"></i>
+						</a>
+						<a title="Mark this answer as best answer"
+						class="vote-accepted mt-2"><i class="fas fa-check fa-2x"></i>
+
+						</a>
+						
+					</div>
 						<div class='media-body m-4'>
 						{!! $answer->body_html !!}
 						<div class="float-right">
