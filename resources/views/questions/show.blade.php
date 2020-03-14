01a30673998cdc9a75a5b7fe4a6bd876b29f1810
unknown <rbrtmt100@gmail.com>
2020-03-14 22:28:07 +0300

    Display answers for question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -51,50 +51,10 @@
             
         </div>
     </div>
-	<div class='row mt-4'>
-		<div class='col-md-12'>
-			<div class='card'>
-				<div class='card-title ml-4 mt-4 mb-0'>
-					<h2>{{$question->answers_count." ".Str::plural('Answer', $question->answers_count)}}</h2>
-				</div>
-				<hr>
-				@foreach($question->answers as $answer)
-					<div class='media'>
-					
-					<div class="d-fex flex-column vote-controls">
-						<a title="This answer is useful" class="vote-up">
-							<i class="fas fa-caret-up fa-3x"></i>
-						</a>
-						<span class="vote-count">1230</span>
-						<a title="This answer is not useful" class="vote-down off">
-							<i class="fas fa-caret-down fa-3x"></i>
-						</a>
-						<a title="Mark this answer as best answer"
-						class="vote-accepted mt-2"><i class="fas fa-check fa-2x"></i>
-
-						</a>
-						
-					</div>
-						<div class='media-body m-4'>
-						{!! $answer->body_html !!}
-						<div class="float-right">
-							<span class="text-muted">Answered {{$answer->created_date}}</span>
-							<div class="media">
-								<a href="{{$answer->user->url}}" class="pr-2">
-								<img src="{{$answer->user->avator}}">
-								</a>
-								<div class="media-body">
-									<a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
-								</div>
-							</div>
-						</div>
-						<hr>
-						</div>
-					</div>
-					
-				@endforeach
-			</div>
-		</div>
-	</div>
+	@include ('answers._index',[
+		'answers'=>$question->answers,
+		'answersCount'=>$question->answers_count,
+	])
+	@include ('answers._create')
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions','QuestionController')->except('show');
+//Route::post('/questions{question}/answers', 'AnswersController@store')->name('answers.store');
+//Route::resource('questions.answers', 'AnswersController')->only(['store','edit', 'update', 'destroy']);
+Route::resource('questions.answers', 'AnswersController')->except(['index','create', 'show']);
 Route::get('/questions/{slug}','QuestionController@show')->name('questions.show');
