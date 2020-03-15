30393a7d57ad7c829f7ac07d6addf8598b416d3a
unknown <rbrtmt100@gmail.com>
2020-03-15 15:38:17 +0300

    Implement delete functionality for answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -33,9 +33,9 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Question $question, Answer $answer)
+   public function edit(Question $question, Answer $answer)
     {
-       $this->authorize('update', $answer);
+        $this->authorize('update', $answer);
 
         return view('answers.edit', compact('question', 'answer'));
     }
@@ -47,25 +47,29 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,Question $question, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
 
         $answer->update($request->validate([
             'body' => 'required',
         ]));
-		return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
-    }
 
+
+        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
+    }
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy( Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
         //
+		$this->authorize('delete', $answer);
+		$answer->delete();
+		return back()->with('success', "Your answer has been removed");
 	
     }
 }
