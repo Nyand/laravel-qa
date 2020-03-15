e946048180bd9ea1a6862d2456aecfdeb4f8d0e7
unknown <rbrtmt100@gmail.com>
2020-03-15 17:07:33 +0300

    Update best_answer_id on questions table when answerremoved in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -24,16 +24,12 @@ class Answer extends Model
 			
 		});
 		static::deleted(function($answer){
-			$question=$answer->question;
-			$question->decrement('answers_count');
-			if($question->best_answer_ID===$answer->id){
-				$question->best_answer_id=NULL;
-				$question->save();
-			}
-		});
+			$answer->question->decrement('answers_count');
+		
+			});
+		}
 		
 		
-	}
 	public function getCreatedDateAttribute(){
 		return $this->created_at->diffForHumans();
 	}
--- database/migrations/2020_03_05_193517_create_questions_table.php
+++ database/migrations/2020_03_05_193517_create_questions_table.php
@@ -21,7 +21,7 @@ class CreateQuestionsTable extends Migration
 			$table->integer('views')->unsigned()->default(0);
 			$table->integer('answers')->unsigned()->default(0);
 			$table->integer('votes')->default(0);
-			$table->integer('best_answer_id')->unsigned()->nullable();
+			$table->bigInteger('best_answer_id')->unsigned()->nullable();
 			$table->bigInteger('user_id')->unsigned();
             $table->timestamps();
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
