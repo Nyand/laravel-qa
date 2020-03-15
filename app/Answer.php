29fd238477d1ba2af3b1c6b214176ed9f16c1fdb
unknown <rbrtmt100@gmail.com>
2020-03-15 16:00:57 +0300

    Update best_answer_id on question table where an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -24,11 +24,20 @@ class Answer extends Model
 			
 		});
 		static::deleted(function($answer){
-			$answer->question->decrement('answers_count');
+			$question=$answer->question;
+			$question->decrement('answers_count');
+			if($question->best_answer_ID===$answer->id){
+				$question->best_answer_id=NULL;
+				$question->save();
+			}
 		});
+		
 		
 	}
 	public function getCreatedDateAttribute(){
 		return $this->created_at->diffForHumans();
+	}
+	public function getStatusAttribute(){
+		return $this->id===$this->question->best_answer_id? 'vote-accepted':'';
 	}
 }
